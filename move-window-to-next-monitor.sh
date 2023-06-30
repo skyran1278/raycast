95a582c20617bb139d8ccca1f75f19b4b4f9011f
ran <skyran1278@gmail.com>
2023-06-30 21:27:25 +0800

    fix(move-window-to-next-monitor): fix built-in display is not considered as a display
    we need to run the "Next Display" command again

--- move-window-to-next-monitor.sh
+++ move-window-to-next-monitor.sh
@@ -14,16 +14,26 @@
 
 # Execute AppleScript to check if the frontmost window is in full-screen mode
 isFullScreen=$(osascript -e 'tell application "System Events" to get value of attribute "AXFullScreen" of window 1 of (first process whose frontmost is true)')
-echo "isFullScreen: $isFullScreen"
 
 # Check the result and determine if the frontmost window is in full-screen mode
 if [[ $isFullScreen == "true" ]]; then
   # Run the "Toggle Fullscreen" command
   osascript -e 'tell application "System Events" to keystroke "f" using {option down}' \
-            -e "delay 0.5"
+            -e 'delay 1'
 fi
 
 # Run the "Next Display" command
 osascript -e 'tell application "System Events" to key code 124 using {option down, shift down}' \
           -e 'delay 0.2' \
-          -e 'tell application "System Events" to keystroke "f" using {option down}'
+          -e 'tell application "System Events" to keystroke "f" using {option down}' \
+          -e 'delay 1'
+
+# because built-in display is not considered as a display, we need to run the "Next Display" command again
+isFullScreen=$(osascript -e 'tell application "System Events" to get value of attribute "AXFullScreen" of window 1 of (first process whose frontmost is true)')
+if [[ $isFullScreen == "false" ]]; then
+  osascript -e 'tell application "System Events" to key code 124 using {option down, shift down}' \
+            -e 'delay 0.2' \
+            -e 'tell application "System Events" to keystroke "f" using {option down}' \
+            -e 'delay 1' \
+            -e 'tell application "System Events" to keystroke return using {option down}'
+fi

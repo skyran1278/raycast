5d496023b1130ff0b39bfe7acd750f26edc6f282
skyran1278 <skyran1278@gmail.com>
2023-07-02 17:23:34 +0800

    feat(move-window-to-next-monitor): use longer delay
    AppleScript and osascript don't provide a built-in way to detect when a window move operation has completed.

--- move-window-to-next-monitor.sh
+++ move-window-to-next-monitor.sh
@@ -24,7 +24,7 @@ fi
 
 # Run the "Next Display" command
 osascript -e 'tell application "System Events" to key code 124 using {option down, shift down}' \
-          -e 'delay 0.2' \
+          -e 'delay 0.5' \
           -e 'tell application "System Events" to keystroke "f" using {option down}' \
           -e 'delay 1'
 
@@ -32,7 +32,7 @@ osascript -e 'tell application "System Events" to key code 124 using {option dow
 isFullScreen=$(osascript -e 'tell application "System Events" to get value of attribute "AXFullScreen" of window 1 of (first process whose frontmost is true)')
 if [[ $isFullScreen == "false" ]]; then
   osascript -e 'tell application "System Events" to key code 124 using {option down, shift down}' \
-            -e 'delay 0.2' \
+            -e 'delay 0.5' \
             -e 'tell application "System Events" to keystroke "f" using {option down}' \
             -e 'delay 1' \
             -e 'tell application "System Events" to keystroke return using {option down}'
